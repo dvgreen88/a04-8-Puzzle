3f656ef4f271922ba721ce865ad2464319aeda4d
dvgreen88 <dvgreen88@yahoo.com>
2015-03-07 14:34:38 -0700

    Create Solver.java

---
+++ src/Solver.java
@@ -0,0 +1,22 @@
+public class Solver {
+   public Solver(Board initial) {
+   // find a solution to the initial board
+   }
+   
+   public boolean isSolvable() {
+   // is the initial board solvable? 
+   }
+   
+   public int moves() {
+   // return min number of moves to solve the initial board;
+   // -1 if no such solution
+   }
+                                  
+   public String toString() {
+   // return string representation of solution (as described above)
+   }
+
+   //  read puzzle instance from stdin and print solution to stdout (in format above)
+   public static void main(String[] args) {
+   }
+}
