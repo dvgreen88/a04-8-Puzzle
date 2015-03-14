049ad57cb50f24b94ec6dbdb91f9fcdcc689aabc
dvgreen88 <dvgreen88@yahoo.com>
2015-03-14 14:22:05 -0600

    Update Solver.java

--- src/Solver.java
+++ src/Solver.java
@@ -5,31 +5,54 @@
 
 package a04_8_Puzzle;
 
-import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdOut;
 
+/**
+ * find a solution to the initial board
+ * 
+ * The constructor should throw a java.lang.IllegalArgumentException if the
+ * initial board is not solvable and a java.lang.NullPointerException if the
+ * initial board is null.
+ * 
+ * @param initial
+ */
 public class Solver {
-	Board initial;
+	private final Stack<Board> boards;
+	private int moves;
+	private static boolean isSolvable;
 
-	/**
-	 * find a solution to the initial board
-	 * 
-	 * The constructor should throw a java.lang.IllegalArgumentException if the
-	 * initial board is not solvable and a java.lang.NullPointerException if the
-	 * initial board is null.
-	 * 
-	 * @param initial
-	 */
-	public Solver(Board initial) {
-		if (initial == null) {
-			throw new java.lang.NullPointerException();
+	private class SearchNode implements Comparable<SearchNode> {
+		private Board board;
+		private int moves;
+		private SearchNode previous;
+		private int cachedPriority = -1;
+
+		SearchNode(Board board, int moves, SearchNode previous) {
+			this.board = board;
+			this.moves = moves;
+			this.previous = previous;
 		}
-		
-		if (!initial.isSolvable()) {
-			throw new java.lang.IllegalArgumentException();
+
+		private int priority() {
+			if (cachedPriority == -1) {
+				cachedPriority = moves + board.manhattan();
+			}
+			return cachedPriority;
 		}
-		this.initial = initial;
+
+		@Override
+		public int compareTo(SearchNode that) {
+			if (this.priority() < that.priority()) {
+				return -1;
+			}
+			if (this.priority() > that.priority()) {
+				return +1;
+			}
+			return 0;
+		}
 	}
 
 	/**
@@ -37,8 +60,8 @@ public class Solver {
 	 * 
 	 * @return true if solvable, otherwise false
 	 */
-	public boolean isSolvable() {
-		return false;
+	public static boolean isSolvable() {
+		return isSolvable;
 	}
 
 	/**
@@ -48,7 +71,19 @@ public class Solver {
 	 * @return min number of moves to solve the board, -1 if no solution
 	 */
 	public int moves() {
-		return 0;
+		if (isSolvable) {
+			return boards.size() - 1;
+		} else {
+			return -1;
+		}
+	}
+
+	public Iterable<Board> solution() {
+		if (isSolvable) {
+			return boards;
+		} else {
+			return null;
+		}
 	}
 
 	/**
@@ -58,35 +93,28 @@ public class Solver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// header
-		StdOut.printf("%-25s %7s %8s\n", "filename", "moves", "time");
-		StdOut.println("------------------------------------------");
+		// create initial board from file
+		In in = new In(args[0]);
+		int N = in.readInt();
+		int[][] blocks = new int[N][N];
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				blocks[i][j] = in.readInt();
+		Board initial = new Board(blocks);
 
-		// for each command-line argument
-		for (String filename : args) {
-			// read in the board specified in the filename
-			In in = new In(filename);
-			int N = in.readInt();
-			int[][] blocks = new int[N][N];
-			for (int i = 0; i < N; i++)
-				for (int j = 0; j < N; j++)
-					blocks[i][j] = in.readInt();
-			Board initial = new Board(blocks);
+		// check if puzzle is solvable; if so, solve it and output solution
+		if (initial.isSolvable()) {
+			Solver solver = new Solver(initial);
+			StdOut.println("Minimum number of moves = " + solver.moves());
 
-			// check if puzzle is solvable; if so, solve it print out number of
-			// moves
-			if (initial.isSolvable()) {
-				Stopwatch timer = new Stopwatch();
-				Solver solver = new Solver(initial);
-				int moves = solver.moves();
-				double time = timer.elapsedTime();
-				StdOut.printf("%-25s %7d %8.2f\n", filename, moves, time);
+			for (Board board : solver.solution()) {
+				StdOut.println(board);
 			}
+		}
 
-			// if not, print that it is unsolvable
-			else {
-				StdOut.printf("%-25s   unsolvable\n", filename);
-			}
+		// if not, report unsolvable
+		else {
+			StdOut.println("Unsolvable puzzle");
 		}
 	}
 }
